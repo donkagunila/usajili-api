c156159195a46c70dff4ac8b107c921863d158a1
donkagunila <donkagunila@gmail.com>
2019-08-31 14:02:29 +0300

    load random photos

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,6 +15,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'avatar' => 'https://i.pravatar.cc/300',
             'username' => $this->username,
             'email'  => $this->email,
             'first_name'  => $this->first_name,
