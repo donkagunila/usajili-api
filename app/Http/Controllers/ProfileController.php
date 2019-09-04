7b58d30271fa555bdba3e11b5cb474826cc8a34a
donkagunila <donkagunila@gmail.com>
2019-09-04 18:20:54 +0300

    Success message after update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Http\Resources\UserResource as UserResource;
 
+use Illuminate\Support\Facades\Validator;
+
 use App\Profile;
 use App\Location;
 use app\Status;
@@ -29,52 +31,99 @@ class ProfileController extends Controller
 
 
     	if ($request->input('first_name')) {
+
     		$profile->update([
 	    		'first_name' => request('first_name'),
 	    	]);
 	    	$profile->save();
+
+            return $this->returnJson('First name', request('first_name'));
+
     	} elseif ($request->input('last_name')) {
     		$profile->update([
 	    		'last_name' => request('last_name'),
 	    	]);
 	    	$profile->save();
+
+            return $this->returnJson('Last name', request('last_name'));
+
     	} elseif ($request->input('date_of_birth')) {
     		$profile->update([
 	    		'date_of_birth' => request('date_of_birth'),
 	    	]);
 	    	$profile->save();
+
+            return $this->returnJson('Date of Birth', request('date_of_birth'));
+
     	} elseif ($request->input('mobile_number')) {
     		$profile->update([
 	    		'mobile_number' => request('mobile_number'),
 	    	]);
 	    	$profile->save();
+
+          return $this->returnJson('Mobile Number', request('mobile_number'));
+
+
     	} elseif ($request->input('gender')) {
     		$status->update([
 	    		'gender' => request('gender'),
 	    	]);
 	    	$status->save();
+
+            return $this->returnJson('Gender', request('gender'));
+
+
     	} elseif ($request->input('marital_status')) {
     		$status->update([
 		    	'marital_status' => request('marital_status'),
 	    	]);
 	    	$status->save();
+
+              return $this->returnJson('Marital Status', request('marital_status'));
+
     	} elseif ($request->input('city')) {
 			$location->update([
 	    		'city' => request('city'),
 	    	]);
 	    	$location->save();
+
+            return $this->returnJson('City', request('city'));
+
+
     	} elseif ($request->input('district')) {
 			$location->update([
 	    		'district' => request('district'),
 	    	]);
 	    	$location->save();
+
+            return $this->returnJson('District', request('district'));
+
     	} elseif ($request->input('ward')) {
 			$location->update([
 	    		'ward' => request('ward'),
 	    	]);
 	    	$location->save();
+
+              return $this->returnJson('Ward', request('ward'));
     	}
 
-    	return new UserResource(Auth::user());
+    }
+
+
+    public function returnJson($item, $value)
+    {
+        return [
+            "status" => "success",
+            "message" => $item." Updated successfully",
+            $item => $value            
+        ];
+    }
+
+    public function returnErrorJson()
+    {
+        return [
+            "status" => "Error",
+            "message" => "Field was not updated",           
+        ];
     }
 }
